bd68c705989bd7bcc54d55338f694d5ee260c9a2
thevpaliy <vpaliy97@gmail.com>
2018-12-30 22:41:55 -0500

    Add update() add() methods

--- merklelib/mtree.py
+++ merklelib/mtree.py
@@ -13,7 +13,7 @@ import math
 from anytree import AnyNode, RenderTree
 from anytree.exporter import DotExporter, JsonExporter
 
-LEFT, RIGHT, ROOT = tuple(range(3))
+LEFT, RIGHT, UNKNOWN = tuple(range(3))
 
 
 def _default_hash(x):
@@ -25,6 +25,10 @@ def _default_hash(x):
 def _pairwise(iterable):
   a = iter(iterable)
   return zip(a, a)
+
+
+def is_power(n):
+  return ((n & (n - 1)) == 0) and n != 0
 
 
 def _get_hash(obj):
@@ -42,7 +46,7 @@ def _get_printable_tree(tree):
     )
   root = tree
   if isinstance(tree, MerkleTree):
-    root = tree.root
+    root = tree._root
   parent = AnyNode(name=root.hash)
   queue = [(root, parent)]
   while len(queue) > 0:
@@ -51,7 +55,9 @@ def _get_printable_tree(tree):
     if left is not None:
       queue.append((left, AnyNode(name=left.hash, parent=par)))
     if right is not None:
-      queue.append((right, AnyNode(name=right.hash, parent=par)))
+      any_node = AnyNode(name=right.hash, parent=par)
+      if not isinstance(right, _EmptyNode):
+        queue.append((right, any_node))
   return parent
 
 
@@ -75,9 +81,11 @@ def beautify(tree):
     print(f'{pre}{node.name}')
 
 
-def _concat(*nodes):
+def _concat(hash, *nodes):
   def __concat(x, y):
-    sum = lambda x, y: _get_hash(x) + _get_hash(y)
+    if isinstance(x, _EmptyNode) or isinstance(y, _EmptyNode):
+      return x.hash
+    sum = lambda x, y: hash(_get_hash(x) + _get_hash(y))
     if isinstance(x, _BaseNode):
       if x.type == LEFT:
         return sum(x, y)
@@ -102,7 +110,7 @@ def verify_tree(tree, root, m):
     raise TypeError(f'Expected MerkleTree, got {type(tree)}')
   if len(tree) < m:
     return False
-  leaves = tree.leaves()
+  leaves = tree.leaves
   index, paths = 0, []
   while m > 0:
     level = 2 ** (m.bit_length() - 1)
@@ -112,23 +120,33 @@ def verify_tree(tree, root, m):
     paths.append(node)
     index += level
     m -= level
-  paths = paths[::-1]
-  hash = tree.hash_algo
   if len(paths) > 1:
+    paths = paths[::-1]
     # order is important !
-    concat = lambda a,b: hash(_concat(b, a))
+    concat = lambda a,b: _concat(tree.algo, b, a)
     new_root = functools.reduce(concat, paths)
   else:
-    new_root = hash(paths[0])
+    new_root = paths[0].hash
   return new_root == root
 
 
-def verify_node(target_hash, proof, hash_algo, root):
+def verify_node(target, proof, hash_algo, root):
   if not callable(hash_algo):
     raise TypeError(f'Expected callable, got {type(hash_algo)}')
-  proof = [target_hash] + proof
-  res = functools.reduce(lambda x,y: hash_algo(_concat(x, y)), proof)
-  return root == res
+  # keep it dry
+  concat = lambda x,y: _concat(hash_algo, x, y)
+  def _calculate_root(target):
+    _proof = [target] + proof
+    return functools.reduce(concat, _proof)
+
+  new_root = _calculate_root(target)
+  # try again if the user forgot to hash the target
+  if new_root != root:
+    try:
+      new_root = _calculate_root(hash_algo(target))
+    except:
+      pass
+  return new_root == root
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -179,12 +197,12 @@ class MerkleNode(_BaseNode):
   def type(self):
     parent = self.parent
     if parent is None:
-      return ROOT
+      return UNKNOWN
     return LEFT if (parent.left is self) else RIGHT
 
   @classmethod
   def combine(cls, left, right, hash):
-    return cls(hash(_concat(left, right)), left, right)
+    return cls(_concat(hash, left, right), left, right)
 
 
 class AuditProof(_BaseNode):
@@ -200,15 +218,25 @@ class _EmptyNode(_BaseNode):
     self.hash = hash
     self.parent = parent
 
+  def get_sibiling(self):
+    parent = self.parent
+    if parent is None:
+      return None
+    if parent.left is self:
+      return parent.right
+    elif parent.right is self:
+      return parent.left
+    return None
+
   @property
   def type(self):
-    # AttributeError if the tree has been modifed manually
-    if self.parent.left is self:
-      return LEFT
-    return RIGHT
+    parent = self.parent
+    if parent is None:
+      return UNKNOWN
+    return LEFT if (parent.left is self) else RIGHT
 
   @classmethod
-  def from(cls, node):
+  def create(cls, node):
     if not isinstance(node, MerkleNode):
       raise TypeError(f'Expected MerkleNode, got {type(node)}')
     return cls(node.hash, node.parent)
@@ -226,27 +254,30 @@ class MerkleTree(object):
       hash_algo = _default_hash
     elif not callable(hash_algo):
       raise TypeError('hash must be a callable')
-    self.hash_algo = hash_algo
+    self._hash_algo = hash_algo
 
   def _build_tree(self, leaves):
-    self._mapping = self.root = None
-    hash = self.hash_algo
+    self._mapping = self._root = None
+    hash = self._hash_algo
     nodes = [MerkleNode(hash(leaf)) for leaf in leaves]
     leaves = list(nodes)
     while len(nodes) > 1:
       if (len(nodes) % 2) != 0:
-        nodes.append(_EmptyNode.from(nodes[-1]))
+        nodes.append(_EmptyNode.create(nodes[-1]))
       nodes = [MerkleNode.combine(l, r, hash) for l, r in _pairwise(nodes)]
     if len(leaves) > 0:
-      self._mapping = { node.hash:node for node in leaves }
-      self.root = nodes[0]
+      mapping = collections.OrderedDict()
+      for leaf in leaves:
+        mapping[leaf.hash] = leaf
+      self._mapping = mapping
+      self._root = nodes[0]
 
   def get_proof(self, leaf):
-    mapping, hash = self._mapping, self.hash_algo
+    mapping, hash = self._mapping, self._hash_algo
     target = mapping.get(leaf, mapping.get(hash(leaf)))
     if not isinstance(target, MerkleNode):
-      raise ValueError('Invalid leaf')
-    root, proof = self.root, []
+      raise KeyError('Invalid leaf value.')
+    root, proof = self._root, []
     while target is not root:
       sibiling = target.get_sibiling()
       audit_prf = AuditProof(sibiling.hash, sibiling.type)
@@ -254,44 +285,87 @@ class MerkleTree(object):
       target = target.parent
     return proof
 
-  def add(self, item):
-    '''
-      Even:
-      - no additional nodes needed
-      - node-sibiling-parent-hash
+  def _rehash(self, node):
+    root, hash = self._root, self._hash_algo
+    # rehash all the nodes to the root
+    while node is not root:
+      parent = node.parent
+      sibiling = node.get_sibiling()
+      if isinstance(sibiling, _EmptyNode):
+        sibiling.hash = node.hash
+      parent.hash = _concat(hash, node, sibiling)
+      node = parent
 
-      Odd:
-      - duplicate the added node
-      - recursively rebuild the tree
-    '''
-    hash, last = self.hash_algo, self._last
-    node = MerkleNode(hash(item))
-    self._mapping[item] = node
-    if len(self._mapping) % 2 == 0:
-      parent = last.parent
-      if parent.right is not last:
-        raise RuntimeError(
-          'Tree is invalid. It may have been modifed manually.'
-        )
-      parent.right = node
-      node.parent = parent
-    #  self._rehash(node)
-    else:
-      expand = _is_power(len(self._mapping) +  1)
-      nodes = [MerkleNode(hash(_concat(node+node)), node, node)]
-      while len(nodes) > 1:
-        if expand:
-          nodes.append(nodes[-1])
-        nodes = [MerkleNode.combine(l, r, hash) for l, r in _pairwise(nodes)]
+  def update(self, old, new):
+    if type(old) != type(new):
+      raise TypeError(
+        'Old and the new value are of different types.'
+        'You should hash them to avoid the exception.'
+      )
+    mapping, hash = self._mapping, self._hash_algo
+    leaf = mapping.get(old)
+    if leaf is None:
+      leaf = mapping.get(hash(old))
+      new = hash(new)
+    if not isinstance(leaf, MerkleNode):
+      raise KeyError('Invalid old value.')
+    leaf.hash = new
+    self._rehash(leaf)
+
+  def add(self, item):
+    mapping, hash, leaves = (
+      self._mapping,
+      self._hash_algo,
+      self.leaves
+    )
+
+    last = leaves[-1]
+    sibiling = last.get_sibiling()
+
+    if isinstance(sibiling, _EmptyNode):
+      sibiling.hash = hash(item)
+      mapping[sibiling.hash] = sibiling
+      self._rehash(sibiling)
+      return
+
+    # TODO: finish the second case
+    raise NotImplemented
 
   @property
   def leaves(self):
-    return self._mapping.values()
+    return list(self._mapping.values())
 
-  def verify(self, target_hash, proof):
-    root, hash = self.root.hash, self.hash_algo
-    return verify_node(target_hash, proof, hash, root)
+  @property
+  def merkle_root(self):
+    if self._root is None:
+      return None
+    return self._root.hash
+
+  @property
+  def algo(self):
+    return self._hash_algo
+
+  @algo.setter
+  def algo(self, hash_algo):
+    self._init_hash_algo(hash_algo)
+
+  def verify(self, target, proof):
+    return verify_node(
+      target,
+      proof,
+      self.algo,
+      self.merkle_root
+    )
+
+  def __len__(self):
+    return len(self._mapping)
 
   def __eq__(self, other):
-    hash, count = other.root.hash, len(other._mapping)
-    return verify_tree(self, hash, count)
+    return verify_tree(
+      self,
+      other.merkle_root,
+      len(other)
+    )
+
+  def __repr__(self):
+    return f'<MerkleTree {self.merkle_root}>'
